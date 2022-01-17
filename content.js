a87575e4978f4d916dc99da71df22dc325e995f9
Steviegt6 <xxlennygamerxx@gmail.com>
2022-01-16 21:03:33 -0800

    Boop.

--- content.js
+++ content.js
@@ -7,12 +7,12 @@ const inject = async (branch, version) => {
 
   const branchURLs = {
     release: 'https://api.goosemod.com/inject.js',
-    dev: 'https://raw.githubusercontent.com/GooseMod/GooseMod/dist-dev/index.js',
+    dev: 'https://raw.githubusercontent.com/bonefire-mod/banefyre/dist-dev/index.js',
     local: 'http://localhost:1234/index.js'
   };
 
-  console.log('[GooseMod for Web] Branch =', branch);
-  console.log('[GooseMod for Web] JS Url =', branchURLs[branch]);
+  console.log('[banefyre-ext] Branch =', branch);
+  console.log('[banefyre-ext] JS Url =', branchURLs[branch]);
   
   const js = await (await fetch(branchURLs[branch])).text(); // JSON.parse(localStorage.getItem('goosemodCoreJSCache'));
 
@@ -22,59 +22,5 @@ const inject = async (branch, version) => {
   
   document.body.appendChild(el);
 
-  console.log('[GooseMod for Web] Injected fetched JS');
+  console.log('[banefyre-ext] Injected fetched JS');
 };
-
-
-// Extension Storage (v10)
-const storageCache = {};
-
-chrome.storage.local.get(null, (data) => {
-  Object.assign(storageCache, data);
-
-  if (Object.keys(storageCache).length === 0 && Object.keys(localStorage).find((x) => x.toLowerCase().startsWith('goosemod'))) { // Nothing stored in Extension storage and something GM in localStorage - migrate from LS to Ext
-    const gmKeys = Object.keys(localStorage).filter((x) => x.toLowerCase().startsWith('goosemod'));
-
-    const setObj = {};
-
-    for (const k of gmKeys) {
-      setObj[k] = localStorage.getItem(k);
-      localStorage.removeItem(k);
-    }
-
-    console.log('[GooseMod For Web] Migrated from localStorage to Extension', setObj);
-
-    Object.assign(storageCache, setObj);
-    chrome.storage.local.set(setObj);
-  }
-
-
-  const el = document.createElement('script');
-
-  el.appendChild(document.createTextNode(`(${inject.toString()})(${JSON.stringify(storageCache['goosemodUntetheredBranch'] || 'release')}, ${JSON.stringify(extVersion)})`));
-
-  document.body.appendChild(el);
-});
-
-
-document.addEventListener('gmes_get', ({ }) => {
-  document.dispatchEvent(new CustomEvent('gmes_get_return', {
-    // Firefox requires cloneInto as doesn't like sending objects from content -> page
-    detail: typeof cloneInto === 'undefined' ? storageCache : cloneInto(storageCache, window) 
-  }));
-});
-
-document.addEventListener('gmes_set', ({ detail: { key, value }}) => {
-  storageCache[key] = value; // Repopulate cache with updated value
-  
-  const obj = {}; // Create object for set
-  obj[key] = value;
-
-  chrome.storage.local.set(obj); // Actually store change
-});
-
-document.addEventListener('gmes_remove', ({ detail: { key }}) => {
-  delete storageCache[key]; // Repopulate cache with updated value
-
-  chrome.storage.local.remove(key); // Actually store change
-});
